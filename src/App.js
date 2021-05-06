209dc599ff2ee8b793b3e373cbb833977b0daab9
shubhamsingh333 <shubhamsingh333333@gmail.com>
2021-05-06 21:09:53 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React,{useState} from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 const App = () => {
-  const [name, setName]=useState("");
-  const[fullName, setfullName]=useState();
+  const [name, setName] = useState('');
+  const [fullName, setfullName] = useState();
 
-  const InputEvent= (event)=>{
-    console.log(event.target.value)
+  const InputEvent = event => {
+    console.log(event.target.value);
     setName(event.target.value);
-  }
+  };
 
-  const onSubmit =() =>{
+  const onSubmit = () => {
     setfullName(name);
-    
-  }
+  };
 
-
-  return(
+  return (
     <>
-    <div>
-    <h1>Hello {fullName}</h1>
-    <input type="text" 
-    placeholder= "Enter your Name"
-    onChange={InputEvent}
-    value={name}/>
-    <button onClick={onSubmit}>click me</button>
-    
-    </div>
+      <div>
+        <h1>Hello {fullName}</h1>
+        <input
+          type="text"
+          placeholder="Enter your Name"
+          onChange={InputEvent}
+          value={name}
+        />
+        <button onClick={onSubmit}>click me</button>
+      </div>
     </>
-
-
   );
-}
+};
 export default App;
